7d9f60dda939e65f16643db3a13e70257179fa92
Krystyna Grzesiak <krygrz11@gmail.com>
2025-04-24 16:16:18 +0200

    exp 5 plotting

--- results/analysis/plotting.R
+++ results/analysis/plotting.R
@@ -1,9 +1,9 @@
+library(targets)
+tar_source()
 
+minmax <- function(x) (x - max(x, na.rm = T))/abs(min(x, na.rm = T) - max(x, na.rm = T))
 
-
-minmax <- function(x) (x - max(x))/abs(min(x) - max(x))
-
-scores <- c("m-I-Score", "DR-I-Score", "energy")
+scores <- c("m-I-Score", "DR-I-Score", "energy", "I-Score-beta")
 
 
 ####### example 1
@@ -55,5 +55,38 @@ p1 <- draw_boxplots(experiment_4, color_plt = "lightsteelblue4")
 p2 <- draw_uniform(X, X_miss)
 
 wrap_experiment(p2, p1)
+
+
+#### ex 5
+
+tar_load(experiment_5)
+tar_load(data_ex5)
+
+X <- data_ex5$X
+X_miss <- data_ex5$X_miss
+
+colnames(X) <- paste0("X", 1:ncol(X))
+colnames(X_miss) <- paste0("X", 1:ncol(X_miss))
+
+plot(X)
+
+draw_boxplots(experiment_5, color_plt = "lightsteelblue4")
+
+norm.nob <- X_miss %>%
+  impute_norm.nob() %>%
+  mutate(missing = is.na(X_miss$X1) | is.na(X_miss$X2),
+         method = "mice norm.nob")
+
+gauss <- X_miss %>%
+  impute_gaussian_indep() %>%
+  mutate(missing = is.na(X_miss$X1) | is.na(X_miss$X2),
+         method = "gaussian independent")
+
+rbind(norm.nob, gauss) %>%
+  ggplot() +
+  geom_point(aes(x = X1, y = X2, col = missing)) +
+  facet_grid(~method) +
+  theme_light()
+
 
 
